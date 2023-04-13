dd4615bd79a1f4eef818e9298553578eaa31df7d
Russell <rnc720@gmail.com>
2023-04-13 17:56:08 -0500

    Issue: Search Bar Error Handling - Added Comments to eventlister function for search button - created alerts for when user inputs blank and when user inputs a pokemon that does not exists - this code was done by Maoyuan and tested by everyone in group.

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,17 +1,42 @@
 // Defining Varibles
 var pokemon = ''
 var pokemonEl = document.getElementById('poke-name')
-var buttonEL = document.getElementById('button-addon2')
+var buttonEl = document.getElementById('button-addon2')
 var searchEl = document.getElementById('search-input')
-var spriteEL = document.getElementById('poke-sprite')
+var spriteEl = document.getElementById('poke-sprite')
 
-buttonEL.addEventListener('click', function () {
-	pokemon = searchEl.value.toLowerCase()
+// buttonEL.addEventListener('click', function () {
+// 	pokemon = searchEl.value.toLowerCase()
+// 	fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
+// 		.then((response) => response.json())
+// 		.then((data) => {
+// 			var pokeName = data.name
+// 			pokemonEl.textContent = pokeName
+// 			spriteEL.setAttribute('src', data.sprites.front_default)
+// 		})
+// })
+
+//Return  ' Please enter the name of a Pokemon. ' if leave empty
+buttonEl.addEventListener('click', function () {
+	pokemon = searchEl.value.toLowerCase().trim()
+	if (pokemon === '') {
+		alert('Please enter the name of a Pokemon.')
+		return
+	}
+	// Return 'Pokemon does not exist.' if no response match
 	fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
-		.then((response) => response.json())
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error('Pokemon does not exist.')
+			}
+			return response.json()
+		})
 		.then((data) => {
 			var pokeName = data.name
 			pokemonEl.textContent = pokeName
-			spriteEL.setAttribute('src', data.sprites.front_default)
+			spriteEl.setAttribute('src', data.sprites.front_default)
+		})
+		.catch((error) => {
+			alert(error.message)
 		})
 })
